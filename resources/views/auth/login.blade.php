77119b0ee74bb0ade9f65684f4a4007f62e2bce2
Naufal <naufal_azmi_mukhlis@teknokrat.ac.id>
2026-07-17 15:13:26 +0700

    Remove icon from login button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,11 +56,6 @@
         </div>
 
         <button type="submit" class="btn-primary" style="margin-top:0.25rem">
-            <svg style="width:17px;height:17px" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                stroke-width="2.2">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
-            </svg>
             Masuk ke Sistem
         </button>
     </form>
